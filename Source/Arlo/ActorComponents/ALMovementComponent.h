13c1a9b900236380cfb1e8ea6bb34c12b37fcb33
Brandon Forbes <reznok@gmail.com>
2023-11-24 05:15:58 -0800

    Initial Commit

---
+++ Source/Arlo/ActorComponents/ALMovementComponent.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "GMCMovementUtilityComponent.h"
+#include "ALMovementComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ARLO_API UALMovementComponent : public UGMC_MovementUtilityCmp
+{
+	GENERATED_BODY()
+public:
+	
+};
